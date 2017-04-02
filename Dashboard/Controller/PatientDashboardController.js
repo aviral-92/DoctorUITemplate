248a2e641f678831da05bbf398651a60e0d4e386
Aviral-92 <aviralmittal2012@gmail.com>
2017-04-02 18:27:59 +0530

    Patient Profile fix

--- Dashboard/Controller/PatientDashboardController.js
+++ Dashboard/Controller/PatientDashboardController.js
@@ -104,8 +104,6 @@ scotchApp.controller('patientHome', function ($scope, $http, $cookieStore, $mdDi
     todoServerResponse.error(function (todoData) {
         alert("failure");
     });
-
-
     $scope.updateTodo = function (todoData) {
 
 
@@ -154,49 +152,50 @@ scotchApp.controller('patientHome', function ($scope, $http, $cookieStore, $mdDi
         serverResponse.error(function (response) {
             popUpCalled.popup('Under maintainance', 'Please try after sometime');
         });
+    }
+});
+
+scotchApp.controller('patientProfile', function ($scope, $cookieStore, fileReader, $http, $window, $interval, popUpCalled, ajaxGetResponse) {
+
+    $scope.url = "#/patientProfile";
+    var getPatients = $cookieStore.get('patientLoginData');
+
+    $scope.patients = getPatients
+    $scope.patients.dob = new Date($scope.patients.dob);
+
+    $scope.uploadPicture = function () {
+        var fileInput = document.getElementById('uploadFile');
+        fileInput.click();
     };
 
-    scotchApp.controller('patientProfile', function ($scope, $cookieStore, fileReader, $http, $window, $interval, popUpCalled, ajaxGetResponse) {
 
-        $scope.url = "#/patientProfile";
-        var getPatients = $cookieStore.get('patientLoginData');
+    $scope.getFile = function () {
+        fileReader.readAsDataUrl($scope.file, $scope)
+            .then(function (result) {
+                $scope.patients.src = result;
+                console.log($scope.file);
+            });
+    };
 
-        $scope.patients = getPatients
-        $scope.patients.dob = new Date($scope.patients.dob);
+    $scope.patients = getPatients
+    $scope.patientUpdate = function (patientUpdateValue) {
+        console.log(patientUpdateValue);
 
-        $scope.uploadPicture = function () {
-            var fileInput = document.getElementById('uploadFile');
-            fileInput.click();
-        };
-
-
-        $scope.getFile = function () {
-            fileReader.readAsDataUrl($scope.file, $scope)
-                .then(function (result) {
-                    $scope.patients.src = result;
-                    console.log($scope.file);
-                });
-        };
-
-        $scope.patients = getPatients
-        $scope.patientUpdate = function (patientUpdateValue) {
-            console.log(patientUpdateValue);
-
-            if ($cookieStore.get('patientLoginData').mobile == patientUpdateValue.mobile) {
-                delete patientUpdateValue.mobile;
-            }
-            if (getPatients.email == patientUpdateValue.email) {
-                delete patientUpdateValue.email;
-            }
-            if (getPatients.adhaar == patientUpdateValue.adhaar) {
-                delete patientUpdateValue.adhaar;
-            }
-            var serverResponseUpdatePatient = ajaxGetResponse.updatePatientProfile(patientUpdateValue);
-            serverResponseUpdatePatient.success(function (updateResponse) {
-                //$scope.successMessage = "Successfully Updated...!!!";
-                /*var patientSuccess = $http
-                    .get('https://doctors.cfapps.io/api/patient/get/'+ $cookieStore.get('patientLoginData').email +'/email');*/
-                /*patientSuccess.success(function(data) {
+        if ($cookieStore.get('patientLoginData').mobile == patientUpdateValue.mobile) {
+            delete patientUpdateValue.mobile;
+        }
+        if (getPatients.email == patientUpdateValue.email) {
+            delete patientUpdateValue.email;
+        }
+        if (getPatients.adhaar == patientUpdateValue.adhaar) {
+            delete patientUpdateValue.adhaar;
+        }
+        var serverResponseUpdatePatient = ajaxGetResponse.updatePatientProfile(patientUpdateValue);
+        serverResponseUpdatePatient.success(function (updateResponse) {
+            //$scope.successMessage = "Successfully Updated...!!!";
+            /*var patientSuccess = $http
+                .get('https://doctors.cfapps.io/api/patient/get/'+ $cookieStore.get('patientLoginData').email +'/email');*/
+            /*patientSuccess.success(function(data) {
 	                $cookieStore.remove('patientLoginData');
 	                $cookieStore.put('patientLoginData', data);
                     
@@ -223,167 +222,166 @@ scotchApp.controller('patientHome', function ($scope, $http, $cookieStore, $mdDi
 	            patientSuccess.error(function(data, status, headers, config) {
                     
                 });*/
-                popUpCalled.popup('Paitent Updated', 'Patient updated successfully...!!!');
-            });
-            serverResponseUpdatePatient.error(function (updateResponse, status, headers, config) {
-                popUpCalled.popup('Under Maintainance', 'Inconvinence regrected...!!!');
-                /*  alert("failure message: " + updateResponse.message);*/
-            });
+            popUpCalled.popup('Paitent Updated', 'Patient updated successfully...!!!');
+        });
+        serverResponseUpdatePatient.error(function (updateResponse, status, headers, config) {
+            popUpCalled.popup('Under Maintainance', 'Inconvinence regrected...!!!');
+            /*  alert("failure message: " + updateResponse.message);*/
+        });
+    }
+
+    //Calucate Age of Patient
+    var age = new Date().getYear() - new Date($scope.patients.dob).getYear();
+    $scope.patients.age = age;
+    //Calculate percentage dynamically...
+    if (getPatients != null) {
+        var field = 5;
+        if (getPatients.patientHomeAddress != null &&
+            getPatients.patientHomeAddress != 'NA') {
+            field++;
         }
-
-        //Calucate Age of Patient
-        var age = new Date().getYear() - new Date($scope.patients.dob).getYear();
-        $scope.patients.age = age;
-        //Calculate percentage dynamically...
-        if (getPatients != null) {
-            var field = 5;
-            if (getPatients.patientHomeAddress != null &&
-                getPatients.patientHomeAddress != 'NA') {
-                field++;
-            }
-            if (getPatients.dob != null &&
-                getPatients.dob != 'NA') {
-                field++;
-            }
-            if (getPatients.gender != null &&
-                getPatients.gender != 'NA') {
-                field++;
-            }
-            if (getPatients.age != null &&
-                getPatients.age != 'NA') {
-                field++;
-            }
-            if (getPatients.description != null &&
-                getPatients.description != 'NA') {
-                field++;
-            }
-            if (getPatients.allergies != null &&
-                getPatients.allergies != 'NA') {
-                field++;
-            }
-            $scope.percent = parseInt((field / 11) * 100) + '%';
+        if (getPatients.dob != null &&
+            getPatients.dob != 'NA') {
+            field++;
         }
-    });
-
-    scotchApp.factory('alert', function ($uibModal) {
-
-        function show(action, event) {
-            return $uibModal.open({
-                templateUrl: '../../Dashboard/calender/PatientModalContent.html',
-                controller: function () {
-                    var vm = this;
-                    vm.action = action;
-                    vm.event = event;
-                },
-                controllerAs: 'vm'
-            });
+        if (getPatients.gender != null &&
+            getPatients.gender != 'NA') {
+            field++;
         }
+        if (getPatients.age != null &&
+            getPatients.age != 'NA') {
+            field++;
+        }
+        if (getPatients.description != null &&
+            getPatients.description != 'NA') {
+            field++;
+        }
+        if (getPatients.allergies != null &&
+            getPatients.allergies != 'NA') {
+            field++;
+        }
+        $scope.percent = parseInt((field / 11) * 100) + '%';
+    }
+});
 
-        return {
-            show: show
-        };
-    });
+scotchApp.factory('alert', function ($uibModal) {
 
-    scotchApp.controller('KitchenSinkCtrl', function (moment, alert, calendarConfig) {
+    function show(action, event) {
+        return $uibModal.open({
+            templateUrl: '../../Dashboard/calender/PatientModalContent.html',
+            controller: function () {
+                var vm = this;
+                vm.action = action;
+                vm.event = event;
+            },
+            controllerAs: 'vm'
+        });
+    }
 
-        var vm = this;
-        //These variables MUST be set as a minimum for the calendar to work
-        vm.calendarView = 'month';
-        vm.viewDate = new Date();
-        var actions = [{
-            label: '',
-            onClick: function (args) {
-                alert.show('Edited', args.calendarEvent);
-            }
+    return {
+        show: show
+    };
+});
+
+scotchApp.controller('KitchenSinkCtrl', function (moment, alert, calendarConfig) {
+
+    var vm = this;
+    //These variables MUST be set as a minimum for the calendar to work
+    vm.calendarView = 'month';
+    vm.viewDate = new Date();
+    var actions = [{
+        label: '',
+        onClick: function (args) {
+            alert.show('Edited', args.calendarEvent);
+        }
             }, {
-            label: '',
-            onClick: function (args) {
-                alert.show('Deleted', args.calendarEvent);
-            }
+        label: '',
+        onClick: function (args) {
+            alert.show('Deleted', args.calendarEvent);
+        }
             }];
-        vm.events = [{
-            title: 'An event ',
-            color: calendarConfig.colorTypes.warning,
-            startsAt: moment().startOf('week').subtract(2, 'days').add(8, 'hours').toDate(),
-            endsAt: moment().startOf('week').add(1, 'week').add(9, 'hours').toDate(),
-            draggable: true,
-            resizable: true,
-            actions: actions
+    vm.events = [{
+        title: 'An event ',
+        color: calendarConfig.colorTypes.warning,
+        startsAt: moment().startOf('week').subtract(2, 'days').add(8, 'hours').toDate(),
+        endsAt: moment().startOf('week').add(1, 'week').add(9, 'hours').toDate(),
+        draggable: true,
+        resizable: true,
+        actions: actions
             }, {
-            title: '<i class="glyphicon glyphicon-asterisk"></i> <span class="text-primary">Another event</span>, with a <i>html</i> title',
-            color: calendarConfig.colorTypes.info,
-            startsAt: moment().subtract(1, 'day').toDate(),
-            endsAt: moment().add(5, 'days').toDate(),
-            draggable: true,
-            resizable: true,
-            actions: actions
+        title: '<i class="glyphicon glyphicon-asterisk"></i> <span class="text-primary">Another event</span>, with a <i>html</i> title',
+        color: calendarConfig.colorTypes.info,
+        startsAt: moment().subtract(1, 'day').toDate(),
+        endsAt: moment().add(5, 'days').toDate(),
+        draggable: true,
+        resizable: true,
+        actions: actions
             }, {
-            title: 'This is a really long event title that occurs on every year',
+        title: 'This is a really long event title that occurs on every year',
+        color: calendarConfig.colorTypes.important,
+        startsAt: moment().startOf('day').add(7, 'hours').toDate(),
+        endsAt: moment().startOf('day').add(19, 'hours').toDate(),
+        recursOn: 'year',
+        draggable: true,
+        resizable: true,
+        actions: actions
+            }];
+
+    vm.cellIsOpen = true;
+
+    vm.addEvent = function () {
+        // Push will be used to push in array of events.
+        //so we will save array in the last in db so that all the operations got over.
+        vm.events.push({
+            title: 'New event',
+            startsAt: moment().startOf('day').toDate(),
+            endsAt: moment().endOf('day').toDate(),
             color: calendarConfig.colorTypes.important,
-            startsAt: moment().startOf('day').add(7, 'hours').toDate(),
-            endsAt: moment().startOf('day').add(19, 'hours').toDate(),
-            recursOn: 'year',
             draggable: true,
-            resizable: true,
-            actions: actions
-            }];
+            resizable: true
+        });
+    };
 
-        vm.cellIsOpen = true;
+    vm.eventClicked = function (event) {
+        alert.show('Clicked', event);
+    };
 
-        vm.addEvent = function () {
-            // Push will be used to push in array of events.
-            //so we will save array in the last in db so that all the operations got over.
-            vm.events.push({
-                title: 'New event',
-                startsAt: moment().startOf('day').toDate(),
-                endsAt: moment().endOf('day').toDate(),
-                color: calendarConfig.colorTypes.important,
-                draggable: true,
-                resizable: true
-            });
-        };
+    vm.eventEdited = function (event) {
+        alert.show('Edited', event);
+    };
 
-        vm.eventClicked = function (event) {
-            alert.show('Clicked', event);
-        };
+    vm.eventDeleted = function (event) {
+        alert.show('Deleted', event);
+    };
 
-        vm.eventEdited = function (event) {
-            alert.show('Edited', event);
-        };
+    vm.eventTimesChanged = function (event) {
+        alert.show('Dropped or resized', event);
+    };
 
-        vm.eventDeleted = function (event) {
-            alert.show('Deleted', event);
-        };
+    vm.toggle = function ($event, field, event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        event[field] = !event[field];
+    };
 
-        vm.eventTimesChanged = function (event) {
-            alert.show('Dropped or resized', event);
-        };
+    vm.timespanClicked = function (date, cell) {
 
-        vm.toggle = function ($event, field, event) {
-            $event.preventDefault();
-            $event.stopPropagation();
-            event[field] = !event[field];
-        };
-
-        vm.timespanClicked = function (date, cell) {
-
-            if (vm.calendarView === 'month') {
-                if ((vm.cellIsOpen && moment(date).startOf('day').isSame(moment(vm.viewDate).startOf('day'))) || cell.events.length === 0 || !cell.inMonth) {
-                    vm.cellIsOpen = false;
-                } else {
-                    vm.cellIsOpen = true;
-                    vm.viewDate = date;
-                }
-            } else if (vm.calendarView === 'year') {
-                if ((vm.cellIsOpen && moment(date).startOf('month').isSame(moment(vm.viewDate).startOf('month'))) || cell.events.length === 0) {
-                    vm.cellIsOpen = false;
-                } else {
-                    vm.cellIsOpen = true;
-                    vm.viewDate = date;
-                }
+        if (vm.calendarView === 'month') {
+            if ((vm.cellIsOpen && moment(date).startOf('day').isSame(moment(vm.viewDate).startOf('day'))) || cell.events.length === 0 || !cell.inMonth) {
+                vm.cellIsOpen = false;
+            } else {
+                vm.cellIsOpen = true;
+                vm.viewDate = date;
             }
+        } else if (vm.calendarView === 'year') {
+            if ((vm.cellIsOpen && moment(date).startOf('month').isSame(moment(vm.viewDate).startOf('month'))) || cell.events.length === 0) {
+                vm.cellIsOpen = false;
+            } else {
+                vm.cellIsOpen = true;
+                vm.viewDate = date;
+            }
+        }
 
-        };
+    };
 
-    });
 });
